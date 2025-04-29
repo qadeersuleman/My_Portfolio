456e8d4c6ec29451f1a67dc6f2fd6f9d11a4fcf7
Qadeer Suleman <abdulqadeermah788@gmail.com>
2025-04-29 07:47:28 -0700

    About us shadow Removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+
 import Main from "./Components/Main";
-import SplashScreen from "./Components/SplashScreen";
+
 
 function App() {
-  
-      const [showSplash, setShowSplash] = useState(true);
-
-return showSplash ? <SplashScreen onFinish={() => setShowSplash(false)} /> : <Main />;
+  return (
+      <Main />
+  )
 
    
 }
